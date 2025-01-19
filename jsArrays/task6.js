ed954676dcb5c78108815821e5e61f95a851c3fc
liudmyla isopenko <l.isopenko@astounddigital.com>
2025-01-19 20:47:15 +0200

    Added solutions for array exercises – 6 practical tasks

---
+++ jsArrays/task6.js
@@ -0,0 +1,5 @@
+const numbersList = [1, 10, 14, 2, 4, 5, 43, 34];
+const sortedNumbers = [...numbersList];
+sortedNumbers.sort((a, b) => a - b);
+console.log("Вихідний масив:", numbersList);
+console.log("Відсортований масив:", sortedNumbers);
